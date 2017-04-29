cb5241d01f1e7d915dbf7e2c30959fdf32467c7f
Bharat Kumar <bharat.159753@gmail.com>
2017-04-29 23:06:06 +0530

    about.html added

--- routes.py
+++ routes.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+  return render_template("index.html")
+
+@app.route("/about")
+def about():
+  return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True)
